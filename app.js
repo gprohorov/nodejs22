dbfc9056553146b753abbc9ab4eb3786f0e894c6
gprohorov <george.prokhorov@external.getneteurope.com>
2022-09-12 12:36:45 +0300

    routing

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import items from './data/items.json';
+import _ from 'lodash';
+
+const BASE_ITEMS_URL = '/api/v1/items';
 
 const server = express();
 const PORT = 3000;
@@ -10,6 +14,20 @@ server.get('/', (req, res) =>{
     console.log(" Request to root");
     res.send('Hello, browser');
 })
+
+server.get(BASE_ITEMS_URL, (req, res) =>{
+    res.json(items);
+});
+
+server.get(BASE_ITEMS_URL+'/:id', (req, res) =>{
+    const id = req.params.id;
+    const item = _.find(items, item => item.id === id);
+    if (item) {
+        res.json(item);
+    }else {
+        res.send('NOT FOUND');
+    }
+});
 
 server.listen(3000, () => {
     console.log('Server is listening on port ' + PORT);
