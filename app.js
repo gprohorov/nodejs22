7a2795f2e9a7b4a6bf191145eb3a9dc909d31942
gprohorov <george.prokhorov@external.getneteurope.com>
2022-09-25 22:48:54 +0300

    ER

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import items from './data/items.json';
 import _ from 'lodash';
+import ItemRoute from './routes/ItemRoute';
 
 const BASE_ITEMS_URL = '/api/v1/items';
 
 const server = express();
+server.use(BASE_ITEMS_URL, ItemRoute);
+
+
 const PORT = 3000;
 const message = "Hello, ES6 !!!11111111111!!!";
 console.log("hello node.js");
@@ -15,19 +19,6 @@ server.get('/', (req, res) =>{
     res.send('Hello, browser');
 })
 
-server.get(BASE_ITEMS_URL, (req, res) =>{
-    res.json(items);
-});
-
-server.get(BASE_ITEMS_URL+'/:id', (req, res) =>{
-    const id = req.params.id;
-    const item = _.find(items, item => item.id === id);
-    if (item) {
-        res.json(item);
-    }else {
-        res.send('NOT FOUND');
-    }
-});
 
 server.listen(3000, () => {
     console.log('Server is listening on port ' + PORT);
---
+++ routes/ItemRoute.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import items from '../data/items.json';
+import _ from 'lodash';
+
+const router = express.Router();
+
+
+router.get('/', (req, res) =>{
+    res.json(items);
+});
+
+router.get('/:id', (req, res) =>{
+    const id = req.params.id;
+    const item = _.find(items, item => item.id === id);
+    if (item) {
+        res.json(item);
+    }else {
+        res.send('NOT FOUND');
+    }
+});
+
+module.exports = router;
